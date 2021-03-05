5537c70d93791aaa7dc8199f0f48c41a03270ff7
Adam Wathan <adam.wathan@gmail.com>
2021-03-05 16:31:50 -0500

    Fix font variant numeric plugin

--- src/corePlugins/fontVariantNumeric.js
+++ src/corePlugins/fontVariantNumeric.js
@@ -1,6 +1,5 @@
-let fontVariantBaseStyles = [
-  '.ordinal, .slashed-zero, .lining-nums, .oldstyle-nums, .proportional-nums, .tabular-nums, .diagonal-fractions, .stacked-fractions',
-  {
+let fontVariantBaseStyles = {
+  '.ordinal, .slashed-zero, .lining-nums, .oldstyle-nums, .proportional-nums, .tabular-nums, .diagonal-fractions, .stacked-fractions': {
     '--tw-ordinal': 'var(--tw-empty,/*!*/ /*!*/)',
     '--tw-slashed-zero': 'var(--tw-empty,/*!*/ /*!*/)',
     '--tw-numeric-figure': 'var(--tw-empty,/*!*/ /*!*/)',
@@ -9,32 +8,32 @@ let fontVariantBaseStyles = [
     'font-variant-numeric':
       'var(--tw-ordinal) var(--tw-slashed-zero) var(--tw-numeric-figure) var(--tw-numeric-spacing) var(--tw-numeric-fraction)',
   },
-]
+}
 
 module.exports = function ({ jit: { addUtilities } }) {
   addUtilities({
-    'normal-nums': [fontVariantBaseStyles],
-    ordinal: [fontVariantBaseStyles],
-    'slashed-zero': [fontVariantBaseStyles],
-    'lining-nums': [fontVariantBaseStyles],
-    'oldstyle-nums': [fontVariantBaseStyles],
-    'proportional-nums': [fontVariantBaseStyles],
-    'tabular-nums': [fontVariantBaseStyles],
-    'diagonal-fractions': [fontVariantBaseStyles],
-    'stacked-fractions': [fontVariantBaseStyles],
+    'normal-nums': fontVariantBaseStyles,
+    ordinal: fontVariantBaseStyles,
+    'slashed-zero': fontVariantBaseStyles,
+    'lining-nums': fontVariantBaseStyles,
+    'oldstyle-nums': fontVariantBaseStyles,
+    'proportional-nums': fontVariantBaseStyles,
+    'tabular-nums': fontVariantBaseStyles,
+    'diagonal-fractions': fontVariantBaseStyles,
+    'stacked-fractions': fontVariantBaseStyles,
   })
 
   addUtilities({
-    'normal-nums': [['.normal-nums', { 'font-variant-numeric': 'normal' }]],
-    ordinal: [['.ordinal', { '--tw-ordinal': 'ordinal' }]],
-    'slashed-zero': [['.slashed-zero', { '--tw-slashed-zero': 'slashed-zero' }]],
-    'lining-nums': [['.lining-nums', { '--tw-numeric-figure': 'lining-nums' }]],
-    'oldstyle-nums': [['.oldstyle-nums', { '--tw-numeric-figure': 'oldstyle-nums' }]],
-    'proportional-nums': [['.proportional-nums', { '--tw-numeric-spacing': 'proportional-nums' }]],
-    'tabular-nums': [['.tabular-nums', { '--tw-numeric-spacing': 'tabular-nums' }]],
-    'diagonal-fractions': [
-      ['.diagonal-fractions', { '--tw-numeric-fraction': 'diagonal-fractions' }],
-    ],
-    'stacked-fractions': [['.stacked-fractions', { '--tw-numeric-fraction': 'stacked-fractions' }]],
+    'normal-nums': { '.normal-nums': { 'font-variant-numeric': 'normal' } },
+    ordinal: { '.ordinal': { '--tw-ordinal': 'ordinal' } },
+    'slashed-zero': { '.slashed-zero': { '--tw-slashed-zero': 'slashed-zero' } },
+    'lining-nums': { '.lining-nums': { '--tw-numeric-figure': 'lining-nums' } },
+    'oldstyle-nums': { '.oldstyle-nums': { '--tw-numeric-figure': 'oldstyle-nums' } },
+    'proportional-nums': { '.proportional-nums': { '--tw-numeric-spacing': 'proportional-nums' } },
+    'tabular-nums': { '.tabular-nums': { '--tw-numeric-spacing': 'tabular-nums' } },
+    'diagonal-fractions': {
+      '.diagonal-fractions': { '--tw-numeric-fraction': 'diagonal-fractions' },
+    },
+    'stacked-fractions': { '.stacked-fractions': { '--tw-numeric-fraction': 'stacked-fractions' } },
   })
 }
