55eb739ea7ad04874093c045b9dfb7a5c43d0769
Adam Wathan <adam.wathan@gmail.com>
2021-03-27 19:23:13 -0400

    Test important modifier when using prefix

--- tests/15-important-modifier-prefix.test.js
+++ tests/15-important-modifier-prefix.test.js
@@ -7,11 +7,12 @@ function run(input, config = {}) {
   return postcss([tailwind(config)]).process(input, { from: path.resolve(__filename) })
 }
 
-test('important modifier', () => {
+test('important modifier with prefix', () => {
   let config = {
     important: false,
+    prefix: 'tw-',
     darkMode: 'class',
-    purge: [path.resolve(__dirname, './14-important-modifier.test.html')],
+    purge: [path.resolve(__dirname, './15-important-modifier-prefix.test.html')],
     corePlugins: { preflight: false },
     theme: {},
     plugins: [],
@@ -24,7 +25,7 @@ test('important modifier', () => {
   `
 
   return run(css, config).then((result) => {
-    let expectedPath = path.resolve(__dirname, './14-important-modifier.test.css')
+    let expectedPath = path.resolve(__dirname, './15-important-modifier-prefix.test.css')
     let expected = fs.readFileSync(expectedPath, 'utf8')
 
     expect(result.css).toMatchCss(expected)
