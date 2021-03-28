3d4476a6032f9969885923d63696c57d72394d6d
Jordan Pittman <jordan@cryptica.me>
2021-03-28 10:32:31 -0400

    Add support for prefix function
    This has the limitation that you must handle selector prefixes. The prefix function will get called with `.ml` before it gets called with `.ml-1`

--- src/lib/setupContext.js
+++ src/lib/setupContext.js
@@ -405,6 +405,10 @@ function buildPluginApi(tailwindConfig, context, { variantList, variantMap, offs
       return identifier
     }
 
+    if (typeof context.tailwindConfig.prefix === 'function') {
+      return prefixSelector(context.tailwindConfig.prefix, `.${identifier}`).substr(1)
+    }
+
     return context.tailwindConfig.prefix + identifier
   }
 
